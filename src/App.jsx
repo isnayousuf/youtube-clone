c34c7e9223e9f0372c4d43e27b5c1998665b8f19
Isna Yousuf <isnayousuf77@gmail.com>
2025-03-17 14:10:41 +0530

    mount components in app

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 import { Route, Routes } from "react-router-dom";
 import Home from "./Containers/Home/Home";
+import Navbar from "./Containers/Navbar/Navbar";
+import Video from "./Containers/Video/Video";
 
 const App = () => {
+  const [isSidebarCollapsed, setIsSidebarCollapsed] = useState(false);
   return (
     <div>
+      <Navbar setIsSidebarCollapsed={setIsSidebarCollapsed} />
       <Routes>
         <Route
           path="/"
-          element={<Home />}
+          element={<Home isSidebarCollapsed={isSidebarCollapsed} />}
         />
+        <Route path="/video/:categoryId/:videoId" element={<Video />} />
       </Routes>
     </div>
   );
