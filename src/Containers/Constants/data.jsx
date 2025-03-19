916336d647856e22b1931439e6db3367e474629b
isna yousuf <isnayousuf77@gmail.com>
2025-03-19 14:02:32 +0530

    fix: add the right side static links

---
+++ src/Containers/Constants/data.jsx
@@ -0,0 +1,295 @@
+import {
+  BookOpenText,
+  Gamepad2,
+  House,
+  MemoryStick,
+  Music,
+  Newspaper,
+  Trophy,
+  Truck,
+  TvMinimalPlay,
+} from "lucide-react";
+
+export const YTCategories = [
+  {
+    label: "Home",
+    value: 0,
+    icon: (
+      <House
+        size={24}
+        strokeWidth={1.5}
+        absoluteStrokeWidth
+        className="side-icon"
+      />
+    ),
+  },
+  {
+    label: "Gaming",
+    value: 20,
+    icon: (
+      <Gamepad2
+        size={24}
+        strokeWidth={1.5}
+        absoluteStrokeWidth
+        className="side-icon"
+      />
+    ),
+  },
+  {
+    label: "Automobiles",
+    value: 2,
+    icon: (
+      <Truck
+        size={24}
+        strokeWidth={1.5}
+        absoluteStrokeWidth
+        className="side-icon"
+      />
+    ),
+  },
+  {
+    label: "Sports",
+    value: 17,
+    icon: (
+      <Trophy
+        size={24}
+        strokeWidth={1.5}
+        absoluteStrokeWidth
+        className="side-icon"
+      />
+    ),
+  },
+  {
+    label: "Entertainment",
+    value: 24,
+    icon: (
+      <TvMinimalPlay
+        size={24}
+        strokeWidth={1.5}
+        absoluteStrokeWidth
+        className="side-icon"
+      />
+    ),
+  },
+  {
+    label: "Technology",
+    value: 28,
+    icon: (
+      <MemoryStick
+        size={24}
+        strokeWidth={1.5}
+        absoluteStrokeWidth
+        className="side-icon"
+      />
+    ),
+  },
+  {
+    label: "Music",
+    value: 10,
+    icon: (
+      <Music
+        size={24}
+        strokeWidth={1.5}
+        absoluteStrokeWidth
+        className="side-icon"
+      />
+    ),
+  },
+  {
+    label: "People and Blogs",
+    value: 22,
+    icon: (
+      <BookOpenText
+        size={24}
+        strokeWidth={1.5}
+        absoluteStrokeWidth
+        className="side-icon"
+      />
+    ),
+  },
+  {
+    label: "News and Politics",
+    value: 25,
+    icon: (
+      <Newspaper
+        size={24}
+        strokeWidth={1.5}
+        absoluteStrokeWidth
+        className="side-icon"
+      />
+    ),
+  },
+];
+
+
+export const RightSideOption = [
+  {
+    label: "Google Account",
+    value: 0,
+    icon: (
+      <House
+        size={24}
+        strokeWidth={1.5}
+        absoluteStrokeWidth
+        className="side-icon"
+      />
+    ),
+  },
+  {
+    label: "Switch Account",
+    value: 20,
+    icon: (
+      <Gamepad2
+        size={24}
+        strokeWidth={1.5}
+        absoluteStrokeWidth
+        className="side-icon"
+      />
+    ),
+  },
+  {
+    label: "Signout",
+    value: 2,
+    icon: (
+      <Truck
+        size={24}
+        strokeWidth={1.5}
+        absoluteStrokeWidth
+        className="side-icon"
+      />
+    ),
+  },
+  {
+    label: "Youtube Studio",
+    value: 17,
+    icon: (
+      <Trophy
+        size={24}
+        strokeWidth={1.5}
+        absoluteStrokeWidth
+        className="side-icon"
+      />
+    ),
+  },
+  {
+    label: "Purchase and memberships",
+    value: 24,
+    icon: (
+      <TvMinimalPlay
+        size={24}
+        strokeWidth={1.5}
+        absoluteStrokeWidth
+        className="side-icon"
+      />
+    ),
+  },
+  {
+    label: "Your data in Youtube",
+    value: 28,
+    icon: (
+      <MemoryStick
+        size={24}
+        strokeWidth={1.5}
+        absoluteStrokeWidth
+        className="side-icon"
+      />
+    ),
+  },
+  {
+    label: "Appearance: Device theme",
+    value: 10,
+    icon: (
+      <Music
+        size={24}
+        strokeWidth={1.5}
+        absoluteStrokeWidth
+        className="side-icon"
+      />
+    ),
+  },
+  {
+    label: "Language:British English",
+    value: 22,
+    icon: (
+      <BookOpenText
+        size={24}
+        strokeWidth={1.5}
+        absoluteStrokeWidth
+        className="side-icon"
+      />
+    ),
+  },
+  {
+    label: "Restricted Mode: Off",
+    value: 25,
+    icon: (
+      <Newspaper
+        size={24}
+        strokeWidth={1.5}
+        absoluteStrokeWidth
+        className="side-icon"
+      />
+    ),
+  },
+
+  {
+    label: "Location: India",
+    value: 28,
+    icon: (
+      <MemoryStick
+        size={24}
+        strokeWidth={1.5}
+        absoluteStrokeWidth
+        className="side-icon"
+      />
+    ),
+  },
+  {
+    label: "Keyboard shortcuts",
+    value: 10,
+    icon: (
+      <Music
+        size={24}
+        strokeWidth={1.5}
+        absoluteStrokeWidth
+        className="side-icon"
+      />
+    ),
+  },
+  {
+    label: "Settings",
+    value: 22,
+    icon: (
+      <BookOpenText
+        size={24}
+        strokeWidth={1.5}
+        absoluteStrokeWidth
+        className="side-icon"
+      />
+    ),
+  },
+  {
+    label: "Help",
+    value: 25,
+    icon: (
+      <Newspaper
+        size={24}
+        strokeWidth={1.5}
+        absoluteStrokeWidth
+        className="side-icon"
+      />
+    ),
+  },
+  {
+    label: "Send feedback",
+    value: 25,
+    icon: (
+      <Newspaper
+        size={24}
+        strokeWidth={1.5}
+        absoluteStrokeWidth
+        className="side-icon"
+      />
+    ),
+  },
+];
